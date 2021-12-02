ac4a338e79c06e623b9f61be1998a8a5151d635b
Eddy Perez <eddypv0@gmail.com>
2021-12-02 15:58:24 -0400

    all books and all authors

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ import authors from './Data/authors.js'
 import books from './Data/books.js'
 
 const typeDefs = gql `
-  type Person{
-      name:String!
-      phone:String!
-      street:String!
-      city:String!
+  type Book{
+      title:String!
+      published:Int!
+      author:String!
       id:ID!
+      genres:[String!]!
+  }
+  type Author{
+      id:ID!
+      name:String!
+      born:Int
+      bookCount:Int!
   }
   type Query {
       bookCount:Int!
       authorCount:Int!
+      allBooks:[Book!]!
+      allAuthors:[Author]!
   }
 `
 const resolvers = {
     Query:{
         bookCount:() => books.length,
         authorCount:() => authors.length,
+        allBooks:() => books,
+        allAuthors:()  => authors
+    },
+    Author:{
+        bookCount:(root)=>{
+            const count= books.filter(element => element.author == root.name).length;
+            return count
+        }
     }
+
 }
 
 const server = new ApolloServer({typeDefs, resolvers});
